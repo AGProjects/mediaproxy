52c591872af75930b633f50641d72f593784ac59
Ruud Klaver <ruud@ag-projects.com>
2008-04-17 18:16:24 +0000

    Restructured dispatcher.py do use deferreds and use multiple relays properly

--- mediaproxy/dispatcher.py
+++ mediaproxy/dispatcher.py
@@ -11,6 +11,7 @@ import cjson
 
 from twisted.protocols.basic import LineOnlyReceiver
 from twisted.internet.protocol import Factory
+from twisted.internet.defer import Deferred, maybeDeferred
 from twisted.internet import epollreactor
 epollreactor.install()
 from twisted.internet import reactor
@@ -30,6 +31,7 @@ class Config(ConfigSection):
     private_key = None
     ca = None
     port = 12345
+    relay_timeout = 5
 
 
 configuration = ConfigFile(configuration_filename)
@@ -42,10 +44,28 @@ class OpenSERControlProtocol(LineOnlyReceiver):
 
     def lineReceived(self, line):
         if line.strip() == "" and self.line_buf:
-            self.factory.dispatcher.send_command(self.line_buf[0], self.line_buf[1:])
+            defer = self.factory.dispatcher.send_command(self.line_buf[0], self.line_buf[1:])
+            defer.addCallback(self.reply)
+            defer.addErrback(self._relay_error)
+            defer.addErrback(self._catch_all)
             self.line_buf = []
         else:
             self.line_buf.append(line)
+
+    def connectionLost(self, reason):
+        log.debug("Connection to OpenSER lost: %s" % reason.value)
+
+    def reply(self, reply):
+        self.transport.write(reply + "\r\n")
+
+    def _relay_error(self, failure):
+        failure.trap(RelayError)
+        log.error("Error processing request: %s" % failure.value)
+        self.transport.write("error\r\n")
+
+    def _catch_all(self, failure):
+        log.error(failure.getBriefTraceback())
+        self.transport.write("error\r\n")
 
 
 class OpenSERControlFactory(Factory):
@@ -53,27 +73,37 @@ class OpenSERControlFactory(Factory):
 
     def __init__(self, dispatcher):
         self.dispatcher = dispatcher
-        self.prot = None
 
-    def buildProtocol(self, addr):
-        self.prot = Factory.buildProtocol(self, addr)
-        return self.prot
 
-    def reply(self, reply):
-        if self.prot:
-            self.prot.transport.write(reply + "\r\n")
+class RelayError(Exception):
+    pass
 
 
 class RelayServerProtocol(LineOnlyReceiver):
 
     def __init__(self):
         self.command_sent = None
-        self.replied = True
+        self.defer = None
+        self.timer = None
+    
+    def connectionMade(self):
+        self.ip = self.transport.getPeer().host
 
     def send_command(self, command, headers):
-        self.replied = False
+        log.debug('Issuing "%s" command to relay at %s' % (command, self.ip))
+        self.defer = Deferred()
+        self.timer = reactor.callLater(Config.relay_timeout, self.defer.errback, RelayError("Relay at %s timed out" % self.ip))
+        self.defer.addBoth(self._defer_cleanup)
         self.command_sent = command
         self.transport.write("\r\n".join([command] + headers + ["", ""]))
+        return self.defer
+
+    def _defer_cleanup(self, result):
+        if self.timer.active():
+            self.timer.cancel()
+        self.timer = None
+        self.defer = None
+        return result
 
     def lineReceived(self, line):
         line_split = line.split(" ", 1)
@@ -81,29 +111,34 @@ class RelayServerProtocol(LineOnlyReceiver):
             try:
                 stats = cjson.decode(line_split[1])
             except cjson.DecodeError:
-                log.error("Error decoding JSON from relay")
+                log.error("Error decoding JSON from relay at %s" % self.ip)
             else:
                 self.factory.dispatcher.update_statistics(stats)
-        if self.replied:
+                del self.factory.sessions[stats["call_id"]]
             return
-        self.replied = True
-        if self.command_sent == "remove":
+        if self.defer is None:
+            log.error("Got unexpected response from relay at %s: %s" % (self.ip, line))
+            return
+        if line_split[0] == "error":
+            self.defer.errback(RelayError('Received error from relay at %s in response to "%s" command: %s' % (self.ip, self.command_sent, line_split[1])))
+        elif self.command_sent == "remove":
             try:
                 stats = cjson.decode(line)
             except cjson.DecodeError:
-                log.error("Error decoding JSON from relay")
-                self.factory.dispatcher.openser.reply("error")
+                log.error("Error decoding JSON from relay at %s" % self.ip)
             else:
                 self.factory.dispatcher.update_statistics(stats)
-                self.factory.dispatcher.openser.reply("removed")
+                del self.factory.sessions[stats["call_id"]]
+            self.defer.callback("removed")
         else: # update command
-            self.factory.dispatcher.openser.reply(line)
+            self.defer.callback(line)
 
     def connectionLost(self, reason):
-        log.debug("Relay from %s disconnected" % self.transport.getPeer().host)
-        if not self.replied:
-            self.factory.dispatcher.openser.reply("error")
+        log.debug("Relay at %s disconnected" % self.ip)
         self.factory.protocols.remove(self)
+        if self.defer is not None:
+            self.defer.errback(RelayError("Relay at %s disconnected" % self.ip))
+
 
 class RelayFactory(Factory):
     protocol = RelayServerProtocol
@@ -111,19 +146,49 @@ class RelayFactory(Factory):
     def __init__(self, dispatcher):
         self.dispatcher = dispatcher
         self.protocols = []
+        self.sessions = {}
 
     def buildProtocol(self, addr):
-        log.debug("Relay from %s connected" % addr.host)
+        log.debug("Relay at %s connected" % addr.host)
         prot = Factory.buildProtocol(self, addr)
         self.protocols.append(prot)
         return prot
 
     def send_command(self, command, headers):
-        if self.protocols:
-            random.choice(self.protocols).send_command(command, headers)
+        call_id = None
+        for header in headers:
+            if header.startswith("call_id: "):
+                call_id = header.split("call_id: ", 1)[1]
+                break
+        if call_id is None:
+            raise RelayError("Could not parse call_id")
+        if call_id in self.sessions:
+            relay = self.sessions[call_id]
+            if relay not in self.protocols:
+                raise RelayError("Relay for this session is no longer connected")
+            return self.sessions[call_id].send_command(command, headers)
         else:
-            log.error("Could not send command, no relay has connected yet")
-            self.dispatcher.openser.reply("error")
+            try_relays = self.protocols[:]
+            random.shuffle(try_relays)
+            defer = self._try_next(try_relays, command, headers)
+            defer.addCallback(self._add_session, try_relays, call_id)
+            return defer
+
+    def _add_session(self, result, try_relays, call_id):
+        self.sessions[call_id] = try_relays[-1]
+        return result
+
+    def _relay_error(self, failure, try_relays, command, headers):
+        failure.trap(RelayError)
+        log.warn("Relay from %s returned error: %s" % (try_relays.pop().ip, failure.value))
+        return self._try_next(try_relays, command, headers)
+
+    def _try_next(self, try_relays, command, headers):
+        if len(try_relays) == 0:
+            raise RelayError("No suitable relay found")
+        defer = try_relays[-1].send_command(command, headers)
+        defer.addErrback(self._relay_error, try_relays, command, headers)
+        return defer
 
 
 class Dispatcher(object):
@@ -135,12 +200,13 @@ class Dispatcher(object):
         reactor.listenTLS(Config.port, self.relay_factory, self.cred)
         self.openser = OpenSERControlFactory(self)
         reactor.listenUNIX(Config.socket, self.openser)
+        self.defer = None
 
     def run(self):
         reactor.run()
 
     def send_command(self, command, headers):
-        self.relay_factory.send_command(command, headers)
+        return maybeDeferred(self.relay_factory.send_command, command, headers)
 
     def update_statistics(self, stats):
         log.debug("Got the following statistics: %s" % stats)
