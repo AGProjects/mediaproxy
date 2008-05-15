c2e5a38fc254b551a69fb89a0366c262bd7caf37
Ruud Klaver <ruud@ag-projects.com>
2008-05-15 12:58:27 +0000

    Added libiptc source files from iptables-1.4.0 to remove the dependency on libiptc.a

--- setup.py
+++ setup.py
@@ -37,9 +37,14 @@ setup(name         = "mediaproxy",
       scripts      = ['mp-relay', 'mp-dispatcher'],
       ext_modules  = [
           Extension(name = 'mediaproxy.interfaces.system._conntrack',
-                    sources = ['mediaproxy/interfaces/system/_conntrack.c'],
-                    libraries = ["iptc", "netfilter_conntrack"],
-                    define_macros = [('MODULE_VERSION', mediaproxy.__version__)])
+                    sources = ['mediaproxy/interfaces/system/_conntrack.c', 'mediaproxy/interfaces/system/libiptc/libip4tc.c'],
+		    depends = ['mediaproxy/interfaces/system/libiptc/libiptc.c',
+		               'mediaproxy/interfaces/system/libiptc/linux_list.h',
+			       'mediaproxy/interfaces/system/libiptc/include/libiptc/libiptc.h',
+			       'mediaproxy/interfaces/system/libiptc/include/libiptc/ipt_kernel_headers.h'],
+		    include_dirs = ['mediaproxy/interfaces/system/libiptc/include'],
+                    libraries = ["netfilter_conntrack"],
+                    define_macros = [('MODULE_VERSION', mediaproxy.__version__), ('IPTABLES_VERSION', '"1.4.0"')])
       ]
 )
 
