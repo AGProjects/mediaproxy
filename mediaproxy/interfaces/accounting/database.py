e951760aca3e9280cfb78591bbb4140fadaea0c7
Dan Pascu <dan@ag-projects.com>
2008-09-07 04:35:35 +0000

    Do not cache inserted rows to avoid the need to maintain/clean the cache

--- mediaproxy/interfaces/accounting/database.py
+++ mediaproxy/interfaces/accounting/database.py
@@ -34,6 +34,7 @@ configuration.read_settings("Database", Config)
 class MediaSessions(SQLObject):
     class sqlmeta:
         table = Config.sessions_table
+        cacheValues = False
     call_id = StringCol(length=255, dbName=Config.callid_column, notNone=True)
     from_tag = StringCol(length=64, dbName=Config.fromtag_column, notNone=True)
     to_tag = StringCol(length=64, dbName=Config.totag_column, notNone=True)
