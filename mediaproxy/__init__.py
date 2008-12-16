41990493f982d2ae7c78c7a262b4c9bc9944778f
Dan Pascu <dan@ag-projects.com>
2008-12-16 17:35:38 +0000

    Increased version number and updated changelog

--- mediaproxy/__init__.py
+++ mediaproxy/__init__.py
@@ -3,7 +3,7 @@
 
 """Mediaproxy implements a media relay for SIP calls"""
 
-__version__ = "2.2.3"
+__version__ = "2.3.0"
 
 system_config_directory = '/etc/mediaproxy'
 runtime_directory = '/var/run/mediaproxy'
