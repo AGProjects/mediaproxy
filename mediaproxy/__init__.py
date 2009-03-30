6b9817c16fcdbafbde9e20202d9145761d72e267
Dan Pascu <dan@ag-projects.com>
2009-03-30 06:25:08 +0000

    Use LogLevel from python-application datatypes

--- mediaproxy/__init__.py
+++ mediaproxy/__init__.py
@@ -13,10 +13,3 @@ configuration_filename = 'config.ini'
 default_dispatcher_port = 25060
 default_management_port = 25061
 
-class LogLevel(str):
-    def __new__(typ, value):
-        from application import log
-        try:
-            return getattr(log.level, value.upper())
-        except AttributeError:
-            raise ValueError("Not a valid log level: %s" % value)
