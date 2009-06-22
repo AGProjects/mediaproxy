ecb327b407930f4663b7e67986914a5fd94dc909
Ruud Klaver <ruud@ag-projects.com>
2009-06-22 13:28:50 +0000

    Only include used stream types in RADIUS Sip-Applications field

--- mediaproxy/interfaces/accounting/radius.py
+++ mediaproxy/interfaces/accounting/radius.py
@@ -111,7 +111,7 @@ class RadiusAccounting(EventQueue, pyrad.client.Client):
         attrs["Sip-To-Tag"] = stats["to_tag"]
         attrs["NAS-IP-Address"] = stats["streams"][0]["caller_local"].split(":")[0]
         attrs["Sip-User-Agents"] = (stats["caller_ua"] + "+" + stats["callee_ua"])[:253]
-        attrs["Sip-Applications"] = ', '.join(sorted(set(stream['media_type'] for stream in stats['streams'])))[:253]
+        attrs["Sip-Applications"] = ', '.join(sorted(set(stream['media_type'] for stream in stats['streams'] if stream['start_time'] != stream['end_time'])))[:253]
         attrs["Media-Codecs"] = ', '.join(stream['caller_codec'] for stream in stats['streams'])[:253]
         if stats["timed_out"]:
             attrs["Media-Info"] = "timeout"
