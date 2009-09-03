25b38d90449ea931eb2c22cf4187d5098697612b
Dan Pascu <dan@ag-projects.com>
2009-09-03 10:58:15 +0000

    Improved warning message clarity

--- mediaproxy/dispatcher.py
+++ mediaproxy/dispatcher.py
@@ -449,7 +449,7 @@ class RelayFactory(Factory):
     def _relay_error(self, failure, try_relays, command, headers):
         failure.trap(RelayError)
         failed_relay = try_relays.popleft()
-        log.warn("Relay at %s returned error: %s" % (failed_relay.ip, failure.value))
+        log.warn("relay failure: %s" % failure.value)
         return self._try_next(try_relays, command, headers)
 
     def _try_next(self, try_relays, command, headers):
