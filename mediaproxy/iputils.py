f94417e6a5641c23e885d437db91b1189e23922a
Saul Ibarra <saul@ag-projects.com>
2014-02-12 11:39:19 +0000

    Add test IP ranges to non routable ranges list

--- mediaproxy/iputils.py
+++ mediaproxy/iputils.py
@@ -7,23 +7,27 @@ __all__ = ["is_routable_ip"]
 
 import socket
 import struct
+
 from application.configuration.datatypes import NetworkRangeList
+
 
 # Non routable network addresses (RFC 3330)
 #
-_non_routable_netlist = """
-0.0.0.0/8
-10.0.0.0/8
-127.0.0.0/8
-169.254.0.0/16
-172.16.0.0/12
-192.0.2.0/24
-192.168.0.0/16
-224.0.0.0/4
-255.255.255.255/32
-"""
+_non_routable_netlist = [
+    '0.0.0.0/8',
+    '10.0.0.0/8',
+    '127.0.0.0/8',
+    '169.254.0.0/16',
+    '172.16.0.0/12',
+    '192.0.2.0/24',
+    '192.168.0.0/16',
+    '198.51.100.0/24',
+    '203.0.113.0/24',
+    '224.0.0.0/4',
+    '255.255.255.255/32'
+]
 
-_non_routable_nets = NetworkRangeList(','.join(_non_routable_netlist.strip().split('\n')))
+_non_routable_nets = NetworkRangeList(_non_routable_netlist)
 
 
 def is_routable_ip(ip):
