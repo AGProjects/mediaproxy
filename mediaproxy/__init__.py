419cb9a6e754915d7312e3243924b80468403a49
Dan Pascu <dan@ag-projects.com>
2016-01-06 10:27:24 +0000

    Removed runtime dependency check

--- mediaproxy/__init__.py
+++ mediaproxy/__init__.py
@@ -10,22 +10,3 @@ runtime_directory = '/var/run/mediaproxy'
 
 configuration_filename = 'config.ini'
 
-
-package_requirements = {'python-application': '1.2.8',
-                        'python-gnutls':      '1.1.8',
-                        'twisted':            '2.5.0'}
-
-try:
-    from application.dependency import ApplicationDependencies, DependencyError
-except ImportError:
-    class DependencyError(Exception): pass
-
-    class ApplicationDependencies(object):
-        def __init__(self, *args, **kw):
-            pass
-        def check(self):
-            required_version = package_requirements['python-application']
-            raise DependencyError("need python-application version %s or higher but it's not installed" % required_version)
-
-dependencies = ApplicationDependencies(**package_requirements)
-
