6ac46edbd51f392b5e1d211a18830ed303c2989e
Dan Pascu <dan@ag-projects.com>
2017-04-25 14:22:57 +0000

    Updated list of non-routable IP addresses and the corresponding RFC

--- mediaproxy/iputils.py
+++ mediaproxy/iputils.py
@@ -11,19 +11,24 @@ from application.configuration.datatypes import NetworkRangeList
 from mediaproxy.configuration import RelayConfig
 
 
-# Non routable network addresses (RFC 3330)
+# Non routable network addresses (RFC 6890)
 #
 _non_routable_netlist = [
     '0.0.0.0/8',
     '10.0.0.0/8',
+    '100.64.0.0/10',
     '127.0.0.0/8',
     '169.254.0.0/16',
     '172.16.0.0/12',
+    '192.0.0.0/24',
     '192.0.2.0/24',
+    '192.88.99.0/24',
     '192.168.0.0/16',
+    '198.18.0.0/15',
     '198.51.100.0/24',
     '203.0.113.0/24',
     '224.0.0.0/4',
+    '240.0.0.0/4',
     '255.255.255.255/32'
 ]
 
